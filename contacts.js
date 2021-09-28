a805c620f547edd802fcc3c0b9f303eb8fe5ea2e
MishaDreamer-web <shondayn@gmail.com>
2021-09-28 16:48:14 +0300

    Fix: code reuse in contacts.js

--- contacts.js
+++ contacts.js
@@ -3,10 +3,10 @@ const path = require('path');
 const crypto = require('crypto');
 const { read } = require('fs');
 
-const contactsPath = './db/contacts.json';
+const contactsPath = path.join(__dirname, './db/contacts.json');
 
 const readContacts = async () => {
-  const result = await fs.readFile(path.join(__dirname, contactsPath), 'utf8');
+  const result = await fs.readFile(contactsPath, 'utf8');
   const contacts = JSON.parse(result);
   return contacts;
 };
@@ -28,10 +28,7 @@ async function removeContact(contactId) {
 
   const newContacts = contacts.filter(contact => contact.id !== contactId);
 
-  await fs.writeFile(
-    path.join(__dirname, contactsPath),
-    JSON.stringify(newContacts, null, 2),
-  );
+  await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
 
   return removeContact;
 }
@@ -41,10 +38,7 @@ async function addContact(name, email, phone) {
   const newContact = { id: crypto.randomUUID(), name, email, phone };
   contacts.push(newContact);
 
-  await fs.writeFile(
-    path.join(__dirname, contactsPath),
-    JSON.stringify(contacts, null, 2),
-  );
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return newContact;
 }
 
